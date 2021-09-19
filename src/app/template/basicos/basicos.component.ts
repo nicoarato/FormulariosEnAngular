e1b93a37f15e3cd6e5b2ec1287c0e3843488449d
Nico Arato <nico.arato@gmail.com>
2021-09-19 20:28:51 -0300

    Se aplica validación por medio de una directiva personalizada simple

--- src/app/template/basicos/basicos.component.ts
+++ src/app/template/basicos/basicos.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
+import { NgForm } from '@angular/forms';
 
 @Component({
   selector: 'app-basicos',
@@ -8,9 +9,27 @@ import { Component, OnInit } from '@angular/core';
 })
 export class BasicosComponent implements OnInit {
 
+
+  @ViewChild('miFormulario') miFormulario!: NgForm;
+
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+
+  nombreValido(): boolean {
+    return this.miFormulario?.controls.producto?.invalid &&
+      this.miFormulario?.controls.producto?.touched
+  }
+
+  precioValido(): boolean {
+    return this.miFormulario?.controls.precio?.touched &&
+      this.miFormulario?.controls.precio?.value < 0
+  }
+
+  guardar() {
+    console.log(this.miFormulario.value)
   }
 
 }
