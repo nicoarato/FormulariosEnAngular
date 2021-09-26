df23b4aba8796c389d70c2aef2957c04f000c1a6
Nico Arato <nico.arato@gmail.com>
2021-09-26 19:54:41 -0300

    Sección de Template: dinamicos

--- src/app/template/dinamicos/dinamicos.component.ts
+++ src/app/template/dinamicos/dinamicos.component.ts
@@ -1,16 +1,50 @@
 import { Component, OnInit } from '@angular/core';
 
+
+interface Persona {
+  nombre: string;
+  favoritos: Favorito[];
+}
+
+interface Favorito {
+  id: number;
+  nombre: string;
+}
+
 @Component({
   selector: 'app-dinamicos',
   templateUrl: './dinamicos.component.html',
   styles: [
   ]
 })
-export class DinamicosComponent implements OnInit {
+export class DinamicosComponent {
+
+  nuevoJuego: string = '';
+  persona: Persona = {
+    nombre: 'Nicolas',
+    favoritos: [
+      { id: 1, nombre: 'Metal Gear' },
+      { id: 2, nombre: 'Medal of Honor' }
+    ]
+  }
 
   constructor() { }
 
-  ngOnInit(): void {
+  guardar() {
+    console.log('Formulario posteado')
   }
 
+  eliminar(index: number) {
+    this.persona.favoritos.splice(index, 1);
+  }
+
+  agregarJuego() {
+    const nuevoFavorito: Favorito = {
+      id: this.persona.favoritos.length + 1,
+      nombre: this.nuevoJuego
+    };
+
+    this.persona.favoritos.push({ ...nuevoFavorito });
+    this.nuevoJuego = '';
+  }
 }
