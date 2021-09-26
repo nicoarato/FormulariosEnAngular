7632bce6d79c1ba171cf7c3a214e2e1a6a107608
Nico Arato <nico.arato@gmail.com>
2021-09-26 19:56:24 -0300

    Seccion de Reactivos: basicos

--- src/app/reactive/basicos/basicos.component.ts
+++ src/app/reactive/basicos/basicos.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-basicos',
@@ -8,9 +9,43 @@ import { Component, OnInit } from '@angular/core';
 })
 export class BasicosComponent implements OnInit {
 
-  constructor() { }
+  // miFormulario: FormGroup = new FormGroup({
+  //   nombre: new FormControl('Rtx 4080Ti'),
+  //   precio: new FormControl(1500),
+  //   existencias: new FormControl(5),
+  // });
 
-  ngOnInit(): void {
+
+  miFormulario: FormGroup = this.fb.group({
+    nombre: ['', [Validators.required, Validators.minLength(3)]],
+    precio: [, [Validators.min(0), Validators.required]],
+    existencias: [, [Validators.min(0), Validators.required]],
+  })
+
+  constructor(private fb: FormBuilder) { }
+
+  ngOnInit() {
+    this.miFormulario.reset({
+      nombre: 'GTi 330',
+      precio: 465,
+      existencias: 10
+    })
+  }
+
+  campoEsValido(campo: string) {
+    return this.miFormulario.controls[campo].errors &&
+      this.miFormulario.controls[campo].touched;
+  }
+
+  guardar() {
+
+    if (this.miFormulario.invalid) {
+      this.miFormulario.markAllAsTouched();
+      return;
+    }
+
+    console.log(this.miFormulario.value)
+    this.miFormulario.reset();
   }
 
 }
